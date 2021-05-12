1afbfea20d3c31bdf4a11ecd46455d2fe51a0987
Charles Lockner <charleslockner@gmail.com>
2021-05-11 17:15:01 -0700

    more plugins

--- install-rust-server.sh
+++ install-rust-server.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+# https://rust-analyzer.github.io/manual.html#building-from-source
 git clone https://github.com/rust-analyzer/rust-analyzer.git && cd rust-analyzer
 cargo xtask install --server
+#/usr/bin/....
+python3 -m pip install --user --upgrade pynvim
